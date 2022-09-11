e9c99d888f2fa02437531494be951aaa3a22a63f
d22 <dilosir22@gmail.com>
2022-09-11 09:43:29 -0700

    IDK why entity doesn't spawn!!!

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -5,7 +5,7 @@ import com.example.examplemod.util.LivingEntityHelper;
 import net.minecraft.client.Minecraft;
 import com.example.examplemod.mobs.EntityForestSpider;
 import net.minecraft.src.BiomeGenBase;
-import net.minecraft.src.EntitySpider;
+import net.minecraft.src.RenderSpider;
 import net.minecraft.src.SpawnListEntry;
 
 public class ExampleMod implements Mod {
@@ -13,10 +13,11 @@ public class ExampleMod implements Mod {
     @Override
     public void init(Minecraft minecraft) {
 
-        System.out.println( BiomeGenBase.seasonalForest.getClass().getName());
-            LivingEntityHelper.addMonster( new SpawnListEntry(EntityForestSpider.class, 100), BiomeGenBase.seasonalForest);
-            LivingEntityHelper.addMonster( new SpawnListEntry(EntitySpider.class, 100), BiomeGenBase.seasonalForest);
+        LivingEntityHelper.addMonster( new SpawnListEntry(EntityForestSpider.class, 100), BiomeGenBase.seasonalForest);
+        LivingEntityHelper.addEntityRenderMapping(EntityForestSpider.class, new RenderSpider());
+        LivingEntityHelper.registerEntity(EntityForestSpider.class, "ForestSpider", 120);
         System.out.println("[Spiders] should be Initialized.");
+
     }
 
     @Override
--- src/main/java/com/example/examplemod/util/LivingEntityHelper.java
+++ src/main/java/com/example/examplemod/util/LivingEntityHelper.java
@@ -1,41 +1,68 @@
 package com.example.examplemod.util;
 
-import net.minecraft.src.BiomeGenBase;
-import net.minecraft.src.BiomeGenForest;
-import net.minecraft.src.SpawnListEntry;
+import net.minecraft.src.*;
 
 import java.lang.reflect.*;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LivingEntityHelper {
 
-   static Field monsterList;
+    static Field ERenderMap;
+    static Method addMappingMethod;
+
 
     static {
         try{
-            Field[] feilds = BiomeGenBase.class.getDeclaredFields();
+            Field[] feilds = RenderManager.class.getDeclaredFields();
             for(Field feild : feilds) {
-                    if(feild.getName().equals("spawnableMonsterList")) {
-                        monsterList = feild;
-                    }
+                if(feild.getName().equals("entityRenderMap")) {
+                    ERenderMap = feild;
+                }
 
             }
-            if(monsterList == null) throw new RuntimeException("Could not find spawnableMonsterList feild!");
-            monsterList.setAccessible(true);
+            if(ERenderMap == null) throw new RuntimeException("Could not find entityRenderMap feild!");
+            ERenderMap.setAccessible(true);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Method[] methods = EntityList.class.getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.getName().equals("addMapping")) {
+                    if (method.getParameterCount() == 3) {
+                        addMappingMethod = method;
+                    }
+                }
+            }
+            if (addMappingMethod == null) throw new RuntimeException("Could not find (EntityList) addMapping method!");
+            addMappingMethod.setAccessible(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addMonster(SpawnListEntry monster, BiomeGenBase biome){
+        biome.getSpawnableList(EnumCreatureType.monster).add(monster);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void addEntityRenderMapping(Class entity, Object renderer){
+        try {
+            if(ERenderMap.get(RenderManager.instance) instanceof HashMap) {
+                ((HashMap) ERenderMap.get(RenderManager.instance)).put(entity, renderer);
+            }else{
+                System.out.println(entity.getName() + "'s renderer could not be added to rendermanager");
+            }
         }catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-    @SuppressWarnings("unchecked")
-    public static void addMonster(SpawnListEntry monster, BiomeGenBase biome){
+
+    public static void registerEntity(Class<? extends Entity> class1, String s, int i) {
         try {
-                if(monsterList.get((biome)) instanceof ArrayList){
-                    ((ArrayList) monsterList.get((biome))).add(monster);
-                }else{
-                    System.out.println(monster.entityClass.getName() + " could not be added to "+biome.biomeName);
-                }
-        }catch (Exception e) {
+            addMappingMethod.invoke(null, class1, s, i);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
