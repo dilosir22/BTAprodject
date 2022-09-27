a68fe22db8bc22a7a4d060ccb9b4b00f774ff020
d22 <dilosir22@gmail.com>
2022-09-26 21:42:04 -0700

    model and ai is fine

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -8,12 +8,21 @@ import com.example.examplemod.mobs.EntityForestSpider;
 import net.minecraft.src.BiomeGenBase;
 import net.minecraft.src.RenderSpider;
 import net.minecraft.src.SpawnListEntry;
+import net.minecraft.src.helper.Textures;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class ExampleMod implements Mod {
 
     @Override
     public void init(Minecraft minecraft) {
-
+        /*try {
+            minecraft.renderEngine.allocateAndSetupTexture(Textures.readImage(new FileInputStream("java/com/example/examplemod/mobs/forestspider.png")));
+        } catch (FileNotFoundException e) {
+            System.out.println("Texture not found");
+            throw new RuntimeException(e);
+        }*/
         LivingEntityHelper.addMonster( new SpawnListEntry(EntityForestSpider.class, 10), BiomeGenBase.seasonalForest);
         LivingEntityHelper.addEntityRenderMapping(EntityForestSpider.class, new RenderForestSpider());
         LivingEntityHelper.registerEntity(EntityForestSpider.class, "ForestSpider", 120);
--- src/main/java/com/example/examplemod/mobs/EntityForestSpider.java
+++ src/main/java/com/example/examplemod/mobs/EntityForestSpider.java
@@ -7,55 +7,43 @@ public class EntityForestSpider extends EntitySpider {
     private boolean hasjumped = false;
     public EntityForestSpider(World world) {
         super(world);
+        this.setSize(1.2F, 2.3F);
         this.moveSpeed = 0.6f;
         this.health = 35;
         this.texture = "/mob/forestspider.png";
     }
 
     @Override
-    public void entityInitOnSpawn() {
-        if (this.worldObj.difficultySetting != 0 && this.rand.nextInt(60 / this.worldObj.difficultySetting) == 0) {
-            EntityArmouredZombie entityzombie = new EntityArmouredZombie(this.worldObj);
-            entityzombie.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
-            this.worldObj.entityJoinedWorld(entityzombie);
-            entityzombie.mountEntity(this);
-        }
-
-    }
+    public void entityInitOnSpawn() {}
 
     @Override
     protected void attackEntity(Entity entity, float f) {
-        if (f > 2.0F && f < 6.0F && this.rand.nextInt(30/ this.worldObj.difficultySetting) == 0) {
-            if (this.onGround) {
-                for(int i = 0; i < 3; i++){
-                    EntitySpider minispider = new EntitySpider(this.worldObj);
-                    minispider.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
-                    this.worldObj.entityJoinedWorld(minispider);
-                }
-
-                double d = entity.posX - this.posX;
-                double d1 = entity.posZ - this.posZ;
-                float f2 = MathHelper.sqrt_double(d * d + d1 * d1);
-                if(hasjumped) {
-                    this.motionX = -1.5 * (d / (double) f2 * 0.5 * 0.800000011920929 + this.motionX * 0.20000000298023224);
-                    this.motionZ = -1.5 * (d1 / (double) f2 * 0.5 * 0.800000011920929 + this.motionZ * 0.20000000298023224);
-                    this.motionY = 0.2000000059604645;
-                    hasjumped = false;
-                }else{
-                    this.motionX = (d / (double)f2 * 0.5 * 0.800000011920929 + this.motionX * 0.20000000298023224);
-                    this.motionZ = (d1 / (double)f2 * 0.5 * 0.800000011920929 + this.motionZ * 0.20000000298023224);
-                    this.motionY = 0.4000000059604645;
-                    hasjumped = true;
-                }
-
-
+        if (this.rand.nextInt(30/ this.worldObj.difficultySetting) == 0) {
+            double d = entity.posX - this.posX;
+            double d1 = entity.posZ - this.posZ;
+            float f2 = MathHelper.sqrt_double(d * d + d1 * d1);
+            if(hasjumped && this.onGround && f > 2.0F && f < 6.0F) {
+                this.motionX = -1.5 * (d / (double) f2 * 0.5 * 0.800000011920929 + this.motionX * 0.20000000298023224);
+                this.motionZ = -1.5 * (d1 / (double) f2 * 0.5 * 0.800000011920929 + this.motionZ * 0.20000000298023224);
+                this.motionY = 0.1000000059604645;
+                hasjumped = false;
+            }else{
+                this.motionX = (d / (double)f2 * 0.5 * 0.800000011920929 + this.motionX * 0.20000000298023224);
+                this.motionZ = (d1 / (double)f2 * 0.5 * 0.800000011920929 + this.motionZ * 0.20000000298023224);
+                this.motionY = 0.4000000059604645;
+                hasjumped = true;
             }
-        } else {
-            if (this.attackTime <= 0 && f < 2.0F && entity.boundingBox.maxY > this.boundingBox.minY && entity.boundingBox.minY < this.boundingBox.maxY) {
-                this.attackTime = 20;
-                entity.attackEntityFrom(this, this.attackStrength, DamageType.COMBAT);
+        }if(this.rand.nextInt(9/this.worldObj.difficultySetting)==0){
+            for(int i = 0; i < 3; i++){
+                EntitySpider minispider = new EntitySpider(this.worldObj);
+                minispider.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
+                this.worldObj.entityJoinedWorld(minispider);
             }
+        }if (this.attackTime <= 0 && f < 1.5F && entity.boundingBox.maxY > this.boundingBox.minY && entity.boundingBox.minY < this.boundingBox.maxY) {
+            this.attackTime = 20;
+            entity.attackEntityFrom(this, this.attackStrength, DamageType.COMBAT);
         }
+
     }
 
     @Override
@@ -76,9 +64,11 @@ public class EntityForestSpider extends EntitySpider {
             int blockLight = this.worldObj.getBlockLightValue(i, j, k);
             if (this.worldObj.currentWeather != null && this.worldObj.currentWeather.doMobsSpawnInDaylight) {
                 blockLight /= 2;
+            }if(this.worldObj.getWorldTime() < 2300 && this.worldObj.getWorldTime() > 2200){
+                return blockLight <= 4 && super.getCanSpawnHere();
+            }else{
+                return false;
             }
-
-            return blockLight <= 4 && super.getCanSpawnHere();
         }
     }
 }
