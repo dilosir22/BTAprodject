88e01a5c641b503fc6c33027692b121785ca3275
d22 <dilosir22@gmail.com>
2022-09-26 22:02:48 -0700

    ai is fine now

--- src/main/java/com/example/examplemod/mobs/EntityForestSpider.java
+++ src/main/java/com/example/examplemod/mobs/EntityForestSpider.java
@@ -5,6 +5,7 @@ import net.minecraft.src.helper.DamageType;
 
 public class EntityForestSpider extends EntitySpider {
     private boolean hasjumped = false;
+    private int minions = 0;
     public EntityForestSpider(World world) {
         super(world);
         this.setSize(1.2F, 2.3F);
@@ -22,22 +23,24 @@ public class EntityForestSpider extends EntitySpider {
             double d = entity.posX - this.posX;
             double d1 = entity.posZ - this.posZ;
             float f2 = MathHelper.sqrt_double(d * d + d1 * d1);
-            if(hasjumped && this.onGround && f > 2.0F && f < 6.0F) {
+            if(hasjumped && f < 4) {
                 this.motionX = -1.5 * (d / (double) f2 * 0.5 * 0.800000011920929 + this.motionX * 0.20000000298023224);
                 this.motionZ = -1.5 * (d1 / (double) f2 * 0.5 * 0.800000011920929 + this.motionZ * 0.20000000298023224);
                 this.motionY = 0.1000000059604645;
                 hasjumped = false;
-            }else{
+            }else {if(this.onGround && f > 2.0F && f < 4.0F){
                 this.motionX = (d / (double)f2 * 0.5 * 0.800000011920929 + this.motionX * 0.20000000298023224);
                 this.motionZ = (d1 / (double)f2 * 0.5 * 0.800000011920929 + this.motionZ * 0.20000000298023224);
                 this.motionY = 0.4000000059604645;
                 hasjumped = true;
-            }
-        }if(this.rand.nextInt(9/this.worldObj.difficultySetting)==0){
-            for(int i = 0; i < 3; i++){
-                EntitySpider minispider = new EntitySpider(this.worldObj);
-                minispider.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
-                this.worldObj.entityJoinedWorld(minispider);
+            }}
+            if(this.rand.nextInt(9/this.worldObj.difficultySetting)==0 && minions <= 10){
+                for(int i = 0; i < 3; i++){
+                    EntitySpider minispider = new EntitySpider(this.worldObj);
+                    minispider.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
+                    this.worldObj.entityJoinedWorld(minispider);
+                    minions++;
+                }
             }
         }if (this.attackTime <= 0 && f < 1.5F && entity.boundingBox.maxY > this.boundingBox.minY && entity.boundingBox.minY < this.boundingBox.maxY) {
             this.attackTime = 20;
